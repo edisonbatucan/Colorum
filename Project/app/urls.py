39e054a275cd0e1c00939bef4476c0afc2a9fa81
Therese Bolabola <terebol0314@gmail.com>
2022-06-09 00:34:48 +0800

    Added Generics API View

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import suv_list, suv_detail, SUVAPIView, SUVDetails
+from .views import suv_list, suv_detail, SUVAPIView, SUVDetails, GenericAPIView
 
 urlpatterns = [
     # path('suvs/', suv_list),
     path('suvs/', SUVAPIView.as_view()),
     # path('suvs/<int:pk>/', suv_detail)
     path('suvs/<int:id>/', SUVDetails.as_view()),
+    path('generic/suvs/<int:id>/', GenericAPIView.as_view()),
 ]
