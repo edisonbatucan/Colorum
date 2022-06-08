077fd61444c58efc85be37bfecb9c3129dd146f6
Edison Batucan <batucan.edison@gmail.com>
2022-06-08 23:25:28 +0800

    Add PickUp

--- Project/app/models.py
+++ Project/app/models.py
@@ -12,3 +12,12 @@ class SUV(models.Model):
 
     def __str__(self):
         return self.car_name
+
+class PickUp(models.Model):
+    car_name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    num_seats = models.IntegerField()
+    wheel_size = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.car_name
