122f36b8e626d632fbfe46f3c956073be8076fdc
Edison Batucan <batucan.edison@gmail.com>
2022-06-09 11:15:46 +0800

    Update

--- Project/app/serializers.py
+++ Project/app/serializers.py
@@ -1,42 +1,17 @@
 import imp
+#from attr import field
 from rest_framework import serializers
 from .models import SUV, PickUp
 
 
-class SUVSerializer(serializers.Serializer):
-    car_name = serializers.CharField(max_length=100)
-    price = serializers.IntegerField()
-    num_seats = serializers.IntegerField()
-    wheel_size = serializers.CharField(max_length=50)
+class SUVSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SUV
+        # fields = ['id', 'car_name', 'price', 'num_seats', 'wheel_size']
+        fields = '__all__'
 
-    def create(self, validated_data):
-        return SUV.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.car_name = validated_data.get('car_name', instance.car_name)
-        instance.price = validated_data.get('price', instance.price)
-        instance.num_seats = validated_data.get(
-            'num_seats', instance.num_seats)
-        instance.wheel_size = validated_data.get(
-            'wheel_size', instance.wheel_size)
-        instance.save()
-        return instance
-
-class PickUpSerializer(serializers.Serializer):
-    car_name = serializers.CharField(max_length=100)
-    price = serializers.IntegerField()
-    num_seats = serializers.IntegerField()
-    wheel_size = serializers.CharField(max_length=50)
-
-    def create(self, validated_data):
-        return SUV.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.car_name = validated_data.get('car_name', instance.car_name)
-        instance.price = validated_data.get('price', instance.price)
-        instance.num_seats = validated_data.get(
-            'num_seats', instance.num_seats)
-        instance.wheel_size = validated_data.get(
-            'wheel_size', instance.wheel_size)
-        instance.save()
-        return instance
+class PickUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PickUp
+        #fields = ['id', 'car_name', 'price', 'num_seats', 'wheel_size']
+        fields = '__all__'
--- Project/app/views.py
+++ Project/app/views.py
@@ -1,21 +1,130 @@
+from cv2 import imread
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-from .models import PickUp
-from .serializers import PickUpSerializer
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import SUV, PickUp
+from .serializers import SUVSerializer, PickUpSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+
 
 # Create your views here.
 
-def pickup_list(request):
-    if request.method == 'GET':
-        pickups = PickUp.objects.all()
-        serializer = PickUpSerializer(pickups, many=True)
-        return JsonResponse(serializer.data, safe= False)
+class SUVViewSet(viewsets.ModelViewSet):
+    serializer_class = SUVSerializer
+    queryset = SUV.objects.all()
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PickUpSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class PickUpViewSet(viewsets.ModelViewSet):
+    serializer_class = PickUpSerializer
+    queryset = PickUp.objects.all()
+
+# class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
+#     serializer_class = SUVSerializer
+#     queryset = SUV.objects.all()
+#     lookup_field = 'id'
+#     # authentication_classes = [SessionAuthentication, BasicAuthentication]
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request, id=None):
+#         if id:
+#             return self.retrieve(request)
+
+#         else:
+#             return self.list(request)
+
+#     def post(self, request):
+#         return self.create(request)
+
+#     def put(self, request, id=None):
+#         return self.update(request, id)
+
+#     def delete(self, request, id):
+#         return self.destroy(request, id)
+
+
+# class SUVAPIView(APIView):
+
+#     def get(self, request):
+#         suvs = SUV.objects.all()
+#         serializer = SUVSerializer(suvs, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = SUVSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class SUVDetails(APIView):
+
+#     def get_object(self, id):
+#         try:
+#             return SUV.objects.get(id=id)
+#         except SUV.DoesNotExist:
+#             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+#     def get(self, request, id):
+#         suv = self.get_object(id)
+#         serializer = SUVSerializer(suv)
+#         return Response(serializer.data)
+
+#     def put(self, request, id):
+#         suv = self.get_object(id)
+#         serializer = SUVSerializer(suv, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, id):
+#         suv = self.get_object(id)
+#         suv.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET', 'POST'])
+# def suv_list(request):
+#     if request.method == 'GET':
+#         suvs = SUV.objects.all()
+#         serializer = SUVSerializer(suvs, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = SUVSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def suv_detail(request, pk):
+#     try:
+#         suv = SUV.objects.get(pk=pk)
+#     except SUV.DoesNotExist:
+#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = SUVSerializer(suv)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = SUVSerializer(suv, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         suv.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
