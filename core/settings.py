b01d91bdc2fecac5440c33087cf65f5c68485b46
kenjisarabosing <kenjisarabosing@users.noreply.github.com>
2022-06-10 14:12:34 +0800

    Added front-end

--- core/settings.py
+++ core/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'vehicle',
     'vehicle_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +133,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
