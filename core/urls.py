c297a8ae59e863143296b47cc7bf71785da08406
Therese Bolabola <terebol0314@gmail.com>
2022-06-10 21:35:51 +0800

    Updates new user model
    Blacklisted Token has not yet been implemented since it needs to be connected to react first.

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vehicle.urls', namespace='vehicle')),
     path('api/', include('vehicle_api.urls', namespace='vehicle_api')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/user/', include('users.urls', namespace='users')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
