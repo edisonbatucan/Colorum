78d5c3dad3b38cf29d2ff33d3201c654176832ed
Edison Batucan <batucan.edison@gmail.com>
2022-06-11 19:47:36 +0800

    Clean

--- Backend/vehicle/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-
-@admin.register(models.Vehicle)
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ('id', 'car_name', 'price', 'num_seats', 'wheel_size', 'car_type', 'person')
--- Backend/vehicle_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from vehicle.models import Vehicle
-
-
-class VehicleSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'person', 'car_name', 'price', 'num_seats', 'wheel_size', 'car_type')
-        model = Vehicle
