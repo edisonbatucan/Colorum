d080dd6c246afa00f47432cf174a001a6bb9d8c9
kenjisarabosing <kenjisarabosing@users.noreply.github.com>
2022-06-11 19:19:56 +0800

    unknown

--- Backend/vehicle_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import VehicleList, VehicleDetail
-
-app_name ='vehicle_api'
-
-urlpatterns =[
-    path('<int:pk>/', VehicleDetail.as_view(), name='detailcreate'),
-    path('', VehicleList.as_view(), name='listcreate'),
-]
