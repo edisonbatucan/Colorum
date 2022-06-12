cbedf96c213034527f860496d8901560740a0f6e
Edison Batucan <batucan.edison@gmail.com>
2022-06-12 22:13:34 +0800

    footer

--- Frontend/vehicleapi/src/components/footer.js
+++ Frontend/vehicleapi/src/components/footer.js
@@ -23,8 +23,8 @@ function Copyright() {
 	return (
 		<Typography variant="body2" color="textSecondary" align="center">
 			{'Copyright © '}
-			<Link color="inherit" href="https://material-ui.com/">
-				Your Website
+			<Link color="inherit" href="http://localhost:3000/">
+				Colorum
 			</Link>{' '}
 			{new Date().getFullYear()}
 			{'.'}
