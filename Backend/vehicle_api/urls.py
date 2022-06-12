7d274b5268c17c25f509af95d8f23a0b76b841f6
Edison Batucan <batucan.edison@gmail.com>
2022-06-12 15:00:21 +0800

    Fix Search

--- Backend/vehicle_api/urls.py
+++ Backend/vehicle_api/urls.py
@@ -1,5 +1,6 @@
-from .views import VehicleList
+from .views import VehicleList, VehicleListDetailfilter, VehicleDetail
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 # from rest_framework_simplejwt.views import (
 #     TokenObtainPairView,
 #     TokenRefreshView,
@@ -8,12 +9,14 @@ from rest_framework.routers import DefaultRouter
 
 app_name ='vehicle_api'
 
-router = DefaultRouter()
-router.register('', VehicleList, basename='vehicle')
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register('', VehicleList, basename='vehicle')
+# router.register('', VehicleListDetailfilter, basename='search')
+# urlpatterns = router.urls
 
 
-# urlpatterns =[
-#     path('<int:pk>/', VehicleDetail.as_view(), name='detailcreate'),
-#     path('', VehicleList.as_view(), name='listcreate'),
-# ]
+urlpatterns =[
+    path('<int:pk>/', VehicleDetail.as_view(), name='detailcreate'),
+    path('search/', VehicleListDetailfilter.as_view(), name='vehiclesearch'),
+    path('', VehicleList.as_view(), name='listcreate'),
+]
--- Backend/vehicle_api/views.py
+++ Backend/vehicle_api/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets
 from rest_framework import filters
 from .serializers import VehicleSerializer
 from vehicle.models import Vehicle
-from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, BasePermission, SAFE_METHODS, IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, BasePermission, SAFE_METHODS, IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 # Create your views here.
@@ -20,7 +20,7 @@ class VehicleUserWritePermission(BasePermission):
 
         return obj.person == request.user
 
-class VehicleList(viewsets.ModelViewSet):
+class VehicleList(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = VehicleSerializer
 
@@ -32,6 +32,54 @@ class VehicleList(viewsets.ModelViewSet):
     def get_queryset(self):
         return Vehicle.objects.all()
 
+# class VehicleDetail(generics.RetrieveAPIView):
+#     serializer_class = VehicleSerializer
+
+#     def get_queryset(self):
+#         item = self.request.query_params.get('id', None)
+#         print(item)
+#         return Vehicle.objects.filter(id=item)
+
+class VehicleListDetailfilter(generics.ListAPIView):
+
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^car_type']
+
+    # '^' Starts-with search.
+    # '=' Exact matches.
+    # '@' Full-text search. (Currently only supported Django's PostgreSQL backend.)
+    # '$' Regex search.
+
+
+class VehicleSearch(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^car_type']
+
+    # def get_object(self, queryset=None, **kwargs):
+    #     item = self.kwargs.get('pk')
+    #     return get_object_or_404(Post, slug=item)
+
+# class VehicleList(generics.ListAPIView):
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = VehicleSerializer
+
+#     def get_queryset(self):
+#         user =  self.request.user
+#         return Vehicle.objects.filter(person=user)
+
+class VehicleDetail(generics.RetrieveAPIView, VehicleUserWritePermission):
+    serializer_class = VehicleSerializer
+
+    def get_queryset(self):
+        id = self.kwargs.get('pk')
+        print(id)
+        return Vehicle.objects.filter(id=id)
+        
 # class VehicleList(viewsets.ModelViewSet):
 #     permission_classes = [IsAuthenticatedOrReadOnly] 
 #     queryset = Vehicle.objects.all()
